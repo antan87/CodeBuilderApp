5729c66d4abe0ff267a2f92b278d890dcb7ca230
Anton Patron <patronant@gmail.com>
2019-10-23 23:36:31 +0200

    First commit

---
+++ CodeBuilderApp/Workspace/Interface/IWorkspaceFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace CodeBuilderApp.Workspace.Interface
+{
+    public interface IWorkspaceFactory<TWorkspace>
+    where TWorkspace : Microsoft.CodeAnalysis.Workspace
+    {
+        Task<TWorkspace> GetWorkspace(string solutionPath);
+    }
+}
---
+++ CodeBuilderApp/Workspace/Interface/IWorkspaceHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeBuilderApp.Workspace.Interface
+{
+    public interface IWorkspaceHandler<TWorkspace>
+    where TWorkspace : Microsoft.CodeAnalysis.Workspace
+
+    {
+        TWorkspace Workspace { get; }
+    }
+}
